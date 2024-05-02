8e6ca4bf53ccb57fa59fbca7c86c14dce30c2403
Anthony <antonio4793@hotmail.com>
2024-05-02 01:51:12 -0500

    Completable future examples

--- src/main/java/org/meli/ExceptionHandling.java
+++ src/main/java/org/meli/ExceptionHandling.java
@@ -3,28 +3,26 @@ package org.meli;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+
+import static jdk.dynalink.linker.support.Guards.isNotNull;
 
 public class ExceptionHandling {
 
     private static Logger log = LoggerFactory.getLogger(ExceptionHandling.class);
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        CompletableFuture<String> firstFiveNumbersFuture1 = CompletableFuture.supplyAsync(() -> logFirstNumbers(5));
-        CompletableFuture<String> firstFiveNumbersFuture2 = CompletableFuture.supplyAsync(() -> logFirstNumbers(5));
-        CompletableFuture<String> firstFiveNumbersFuture3 = CompletableFuture.supplyAsync(() -> logFirstNumbers(5));
+//        var failedCompletableFuture = CompletableFuture.failedFuture(new IllegalArgumentException("A failed future"))
+        var failedCompletableFuture = CompletableFuture.completedFuture("All ok")
+            .handle((a, ex) -> {
+                return Optional.ofNullable(ex)
+                    .map()
+                log.error("An error occurred [error={}]", ex.getMessage(), ex);
+                    return "An error occurred";
+            });
 
-        var voidCompletableFuture = CompletableFuture.allOf(firstFiveNumbersFuture1,
-            firstFiveNumbersFuture2,
-            firstFiveNumbersFuture3);
-        log.info("result={}", voidCompletableFuture.get());
-    }
-
-    public static String logFirstNumbers(Integer n) {
-        for (int i = 0; i < n; i++) {
-            log.info("{}", i + 1);
-        }
-        return "Done";
+        log.info("result={}", failedCompletableFuture.get());
     }
 }
